f4166cc8fa54b1cb352c2452cf664fbe6d62bbf6
Andriikryl <andreyk557@gmail.com>
2023-07-29 19:41:29 +0300

    add form / input / label / buutom to the singUp component

--- src/app/components/singUp/SingUp.tsx
+++ src/app/components/singUp/SingUp.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+"use client";
+
+import React, { useCallback } from "react";
 import style from "./style.module.css";
+import { useForm } from "react-hook-form";
 export default function SingUp() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onValid = useCallback(() => {
+    console.log("onValid");
+  }, []);
+
   return (
     <div className={style.form__box}>
-      <form action=""></form>
+      <form className={style.form} onSubmit={handleSubmit(onValid)}>
+        <h1>form</h1>
+        <div className={style.flex__group}>
+          <label htmlFor="email">email</label>
+          <input type="email" />
+          <p>an erro</p>
+        </div>
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
