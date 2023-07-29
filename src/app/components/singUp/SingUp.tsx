d8d4c53e2ea5fdb686688e5117679d794602dd74
Andriikryl <andreyk557@gmail.com>
2023-07-29 20:37:20 +0300

    add basic styles and validation

--- src/app/components/singUp/SingUp.tsx
+++ src/app/components/singUp/SingUp.tsx
@@ -3,6 +3,7 @@
 import React, { useCallback } from "react";
 import style from "./style.module.css";
 import { useForm } from "react-hook-form";
+import { error } from "console";
 export default function SingUp() {
   const {
     register,
@@ -11,18 +12,55 @@ export default function SingUp() {
     formState: { errors },
   } = useForm();
 
-  const onValid = useCallback(() => {
-    console.log("onValid");
+  const onValid = useCallback((data: unknown) => {
+    console.log("onValid", data);
   }, []);
+
+  console.log("sing up running render the function", errors);
 
   return (
     <div className={style.form__box}>
       <form className={style.form} onSubmit={handleSubmit(onValid)}>
         <h1>form</h1>
         <div className={style.flex__group}>
-          <label htmlFor="email">email</label>
-          <input type="email" />
-          <p>an erro</p>
+          <label className={style.label} htmlFor="email">
+            email
+          </label>
+          <input
+            type="email"
+            {...register("email", { required: "email is required" })}
+          />
+          {errors.email?.message ? (
+            <p className={style.error}>{errors.email?.message as string}</p>
+          ) : null}
+        </div>
+        <div className={style.flex__group}>
+          <label className={style.label} htmlFor="password">
+            password
+          </label>
+          <input
+            type="password"
+            {...register("password", { required: "password is required" })}
+          />
+          {errors.password?.message ? (
+            <p className={style.error}>{errors.password?.message as string}</p>
+          ) : null}
+        </div>
+        <div className={style.flex__group}>
+          <label className={style.label} htmlFor="confirm password">
+            confirm password
+          </label>
+          <input
+            type="password"
+            {...register("confirmPassword", {
+              required: "confirm password is required",
+            })}
+          />
+          {errors.confirmPassword?.message ? (
+            <p className={style.error}>
+              {errors.confirmPassword?.message as string}
+            </p>
+          ) : null}
         </div>
         <button type="submit">submit</button>
       </form>
